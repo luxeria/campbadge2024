62a2425901325a20bdd99d68c66100090a6437c0
woodstock@Kerberos <woodstocke@egoat.ch>
2024-08-08 23:30:15 +0200

    fix Rotate example

--- FirmwareExamples/RotatedText/src/main.cpp
+++ FirmwareExamples/RotatedText/src/main.cpp
@@ -3,57 +3,15 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-
-
-#define LED_BLACK		0
-
-#define LED_RED_VERYLOW 	(3 <<  11)
-#define LED_RED_LOW 		(7 <<  11)
-#define LED_RED_MEDIUM 		(15 << 11)
-#define LED_RED_HIGH 		(31 << 11)
-
-#define LED_GREEN_VERYLOW	(1 <<  5)   
-#define LED_GREEN_LOW 		(15 << 5)  
-#define LED_GREEN_MEDIUM 	(31 << 5)  
-#define LED_GREEN_HIGH 		(63 << 5)  
-
-#define LED_BLUE_VERYLOW	3
-#define LED_BLUE_LOW 		7
-#define LED_BLUE_MEDIUM 	15
-#define LED_BLUE_HIGH 		31
-
-#define LED_ORANGE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW)
-#define LED_ORANGE_LOW		(LED_RED_LOW     + LED_GREEN_LOW)
-#define LED_ORANGE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM)
-#define LED_ORANGE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH)
-
-#define LED_PURPLE_VERYLOW	(LED_RED_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_PURPLE_LOW		(LED_RED_LOW     + LED_BLUE_LOW)
-#define LED_PURPLE_MEDIUM	(LED_RED_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_PURPLE_HIGH		(LED_RED_HIGH    + LED_BLUE_HIGH)
-
-#define LED_CYAN_VERYLOW	(LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_CYAN_LOW		(LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_CYAN_MEDIUM		(LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_CYAN_HIGH		(LED_GREEN_HIGH    + LED_BLUE_HIGH)
-
-#define LED_WHITE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_WHITE_LOW		(LED_RED_LOW     + LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_WHITE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_WHITE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH    + LED_BLUE_HIGH)
-
-
-
-
-
-
 #define LED_PIN 4
 #define LED_COUNT 214
 // Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 #define HORN 0
 #define EYE 1
 
-Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(19, 11, 
+#define LED_MATRIX_WIDTH 19
+#define LED_MATRIX_HEIGHT 11
+Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(LED_MATRIX_WIDTH, LED_MATRIX_HEIGHT, 
   1, 2, // rows, cols - we use 2 cols to get enough LEDs for the scarf
   LED_PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -78,11 +36,11 @@ void setup() {
   matrix->setTextSize(1);
   matrix->show();
 }
-int offset=0;
+int pos=0;
 void loop() {
-    // matrix->clear();
+    pos++;
 
-    offset+=200;
+    matrix->clear();
 
     // set color for matrix first
     setMatrix();
@@ -94,59 +52,50 @@ void loop() {
     setScarfBottom();
 
     matrix->show();
-    delay(10);
+    delay(50);
 }
 
 void setMatrix()
 {
-    int mw = 19;
-    int mh = 11;
-    // for(int i=1;i<LED_COUNT;i++){
-        // int hue = (offset+i*200) % 65535;
-        // matrix->setPixelColor(i, matrix§->gamma32(matrix->ColorHSV(hue)));
-    // }
-    uint8_t size = max(int(mw/8), 1);
-    matrix->clear();
-    matrix->setTextWrap(false);  // we don't wrap text so it scrolls nicely
-    matrix->setTextSize(1);
-    matrix->setRotation(0);
-    for (int8_t x=7; x>=-70; x--) {
-        matrix->clear();
-        matrix->setCursor(x,2);
-        matrix->setTextColor(LED_PURPLE_HIGH);
-        matrix->print("LuxCamp 2024");
-        setEye();
-        setHorn();
-        setScarfBottom();
-        setScarfTop();
-        matrix->show();
-        delay(50);
+    int fontSize = max(int(LED_MATRIX_WIDTH/8), 1);
+    int animation1Frames = 78;
+    int animation2Frames = 11*8*fontSize+1;
+    int animation1Length = animation1Frames;
+    int animation2Length = animation2Frames/2;
+    int step = pos%(animation1Length + animation2Length);
+
+    // horizontal text
+    if (step<animation1Length) {
+        int cursorPos = 7-step;
+        matrix->setCursor(cursorPos,2);
+        matrix->setTextWrap(false);
+        matrix->setTextSize(1);
+        matrix->setRotation(0);
+        matrix->setTextColor(matrix->Color(0,0,255));
+        matrix->print("Annalina :)");
     }
 
-    matrix->setRotation(3);
-    matrix->setTextSize(size);
-    matrix->setTextColor(LED_PURPLE_HIGH);
-    for (int16_t x=8*size; x>=-9*8*size; x--) {
-        matrix->clear();
-        matrix->setCursor(x,mw/2-size*4+1);
+    // vertical text
+    else {
+        step -= animation1Length;
+        int cursorPos = 1*8*fontSize-step;
+        matrix->setCursor(cursorPos*2,LED_MATRIX_WIDTH/2-fontSize*4+1);
+        matrix->setTextWrap(false);
+        matrix->setTextSize(fontSize);
+        matrix->setRotation(3);
+        if (step<(animation2Length/2-10)) {
+            matrix->setTextColor(matrix->Color(0,255,255));
+        }
+        else {
+            matrix->setTextColor(matrix->Color(255,255,0));
+        }
         matrix->print("Spit Happens!");
-        setEye();
-        setHorn();
-        setScarfBottom();
-        setScarfTop();
-        matrix->show();
-	// note that on a big array the refresh rate from show() will be slow enough that
-	// the delay become irrelevant. This is already true on a 32x32 array.
-        delay(50/size);
     }
-    matrix->setRotation(0);
-    matrix->setCursor(0,0);
-    matrix->show();
 }
 
 void setEye()
 {
-    matrix->setPixelColor(EYE, 255,0,0);
+    matrix->setPixelColor(EYE, 0,255,30);
 }
 
 void setHorn()
