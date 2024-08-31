68ddda304a7b157f51e0c5ec1b1a243b35b12e45
Sebastian Wicki <gandro@gmx.net>
2024-08-31 17:01:36 +0200

    Micropython: Add Button to example
    Signed-off-by: Sebastian Wicki <gandro@gmx.net>

--- FirmwareExamples/Micropython/Simple/main.py
+++ FirmwareExamples/Micropython/Simple/main.py
@@ -3,11 +3,14 @@ import machine, neopixel, random, time
 # LED is on Pin 4
 led = machine.Pin(4, machine.Pin.OUT)
 
+# Boot Button is on Pin 0
+btn = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
+
 # There are 214 LEDs
 np = neopixel.NeoPixel(led, 214)
 
-# Shows 5 different random colors for one second each
-for i in range(5):
+# Shows random colors until the boot button is pressed
+while btn.value():
     r = random.getrandbits(5)
     g = random.getrandbits(5)
     b = random.getrandbits(5)
